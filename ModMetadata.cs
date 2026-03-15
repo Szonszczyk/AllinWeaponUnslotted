6385d974be58e91a8bda368285d6a7549a2fbaaa
Szonszczyk <Szonszczyk@users.noreply.github.com>
2026-03-15 16:46:54 +0100

    Updated version

--- ModMetadata.cs
+++ ModMetadata.cs
@@ -8,7 +8,7 @@ namespace AllinWeaponUnslotted
         public override string Name { get; init; } = "All In Weapon: Unslotted";
         public override string Author { get; init; } = "Szonszczyk";
         public override List<string>? Contributors { get; init; }
-        public override SemanticVersioning.Version Version { get; init; } = new("1.0.0");
+        public override SemanticVersioning.Version Version { get; init; } = new("1.1.0");
         public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.4");
         public override List<string>? Incompatibilities { get; init; } = [];
         public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; } = [];
